b8fb8a079e0c52d2b99eb05dd655238519af5db3
jordanferreiraa <jordanferreira69@gmail.com>
2023-03-08 13:51:22 -0300

    more verses

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ function App() {
   const [textoFrase, setTextoFrase] = useState('')
 
   let frases = [
-    'Vistam toda a armadura de Deus, para que possam permanecer firmes contra as estratégias do diabo. Efésios 6:11.',
     'Em vez disso, revistam-se do Senhor Jesus Cristo e não fiquem imaginando formas de satisfazer seus desejos pecaminosos. Romanos 13:14.',
     'Mas os que confiam no Senhor renovam suas forças; voam alto, como águias. Correm e não se cansam, caminham e não desfalecem. Isaías 40:31.',
     'Busquem, em primeiro lugar, o reino de Deus e a sua justiça, e todas essas coisas lhes serão dadas. Mateus 6:33.',
@@ -14,6 +13,14 @@ function App() {
     'Mas quem ouve meu ensino e não o pratica é tão tolo como a pessoa que constrói sua casa sobre a areia. Mateus 7:26.',
     'Porque Deus amou ao mundo de tal maneira que deu seu Filho unigênito, para que todo o que nele crê não pereça, mas tenha a vida eterna. João 3:16.',
     'Eu lhes digo a verdade: quem ouve minha mensagem e crê naquele que me enviou tem a vida eterna. Jamais será condenado, mas já passou da morte para a vida. João 5:24.',
+    'Jesus disse: "Eu sou o caminho, a verdade e a vida. Ninguém vem ao Pai senão por mim". João 14:6',
+    'Estas coisas vos tenho dito para que tenhais paz em mim. No mundo, passais por aflições; mas tende bom ânimo; eu venci o mundo. João 16:33.',
+    'Mas Deus nos prova seu grande amor ao enviar Cristo para morrer por nós quando ainda éramos pecadores. Romanos 5:8.',
+    'Pois o salário do pecado é a morte, mas a dádiva de Deus é a vida eterna em Cristo Jesus, nosso Senhor. Romanos 6:23.',
+    'Se você declarar com sua boca que Jesus é Senhor e crer em seu coração que Deus o ressuscitou dos mortos, será salvo. Romanos 10:9.',
+    'Vocês são salvos pela graça, por meio da fé. Isso não vem de vocês; é uma dádiva de Deus. Efésios 2:8.',
+    'Vistam toda a armadura de Deus, para que possam permanecer firmes contra as estratégias do diabo. Efésios 6:11.',
+    'Se continuamos a pecar deliberadamente depois de ter recebido o conhecimento da verdade, já não há sacrifício que cubra esses pecados. Hebreus 10:26.',
   ];
 
   function quebraBiscoito() {
@@ -58,7 +65,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#333'
   },
   img: {
     width: 230,
